ca27f34b3b946c9799097c473fb87d71b37f96ac
Belinsky-L-V <belinsky.leonid@gmail.com>
2019-12-09 20:22:11 +0300

    finish reworking intcode computer to operate on streams

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,258 +1,27 @@
-{-# LANGUAGE OverloadedStrings, RankNTypes, FlexibleInstances,
-  TypeApplications, ScopedTypeVariables, NamedFieldPuns,
-  RecordWildCards #-}
 module Day5 where
 
-import Control.Monad.ST
-import Data.STRef
-import Control.Monad (when, unless)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Maybe
-import Control.Monad.Trans.Reader
-import Control.Monad.Trans.Except
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
 import qualified Data.Text.Read as Text.Read
-import Data.Vector (Vector, MVector)
-import qualified Data.Vector as Vector
-import qualified Data.Vector.Mutable as MVector
-import Data.Proxy
-import Data.IntMap (IntMap)
-import qualified Data.IntMap as IntMap
+import Intcode
+import qualified Streaming as S
+import qualified Streaming.Prelude as SP
 
-data Halt = Run | Halt deriving (Show, Read, Eq)
-data Mode = Pos | Immed deriving (Show, Read, Eq)
-
-type Code s = MVector s Int
-type Addr = Int
-type Input = Int
-type Output = Int
-type Op s = [Mode] -> Interpreter s ()
-
-data IntcodeOp = IntcodeOp
-  { icopRun :: forall s. Op s
-  , icopParams :: !Int
-  }
-
-data IntcodeVM s = IntcodeVM
-  { halted :: STRef s Halt
-  , code :: Code s
-  , inputBuf :: STRef s [Input]
-  , outputBuf :: STRef s [Output]
-  , instPointer :: STRef s Int
-  }
-
-dumpVM :: forall s. IntcodeVM s -> ST s String
-dumpVM vm@IntcodeVM{..} = do
-  halt <- readSTRef halted
-  code <- Vector.freeze code
-  input <- readSTRef inputBuf
-  output <- readSTRef outputBuf
-  pointer <- readSTRef instPointer
-  return $ unlines [show (halt, pointer), show (input, output), show code]
-
-type Interpreter s = ReaderT (IntcodeVM s) (ExceptT String (ST s))
-
-class ToIntcodeOp t where
-  params_ :: proxy t -> Int
-  toOp_ :: t -> forall s. Op s
-
-instance ToIntcodeOp Int where
-  params_ _ = 1
-  toOp_ x [Pos] = do
-    vm@IntcodeVM{..} <- ask
-    targetAddr <- withMode Immed
-    MVector.write code targetAddr x
-    return ()
-  toOp_ x modes = ReaderT $ \vm@IntcodeVM{..} -> do
-    dump <- lift $ dumpVM vm
-    throwE $ "Mode for writing isn't positional, or leftover modes: " ++ show modes ++ " " ++ dump
-
-instance ToIntcodeOp r => ToIntcodeOp (Int -> r) where
-  params_ _ = 1 + params_ (Proxy @r)
-  toOp_ f [] = ReaderT $ \vm -> do
-    dump <- lift $ dumpVM vm
-    throwE $ "Ran out of modes: " ++ dump
-  toOp_ f (m:ms) = do
-    x <- withMode m
-    toOp_ (f x) ms
-
-toIntcodeOp :: forall f. ToIntcodeOp f => f -> IntcodeOp
-toIntcodeOp f = IntcodeOp (toOp_ f) (params_ (Proxy @f))
-
-withMode :: Mode -> Interpreter s Int
-withMode Pos = ReaderT $ \vm@IntcodeVM{..} -> do
-  arg <- lift $ MVector.read code =<< MVector.read code =<< readSTRef instPointer
-  lift $ modifySTRef instPointer succ
-  return arg
-withMode Immed = ReaderT $ \vm@IntcodeVM{..} -> do
-  arg <- lift $ MVector.read code =<< readSTRef instPointer
-  lift $ modifySTRef instPointer succ
-  return arg
-
-inputOp :: IntcodeOp
-inputOp = IntcodeOp inputOp' 1
-  where
-    inputOp' :: forall s. Op s
-    inputOp' [Pos] = do
-      vm@IntcodeVM{inputBuf, code} <- ask
-      (x:rest) <- lift . lift $ readSTRef inputBuf
-      writeAddr <- withMode Immed
-      MVector.write code writeAddr x
-      lift . lift $ modifySTRef inputBuf tail
-      return ()
-    inputOp' modes = ReaderT $ \vm -> do
-      dump <- lift $ dumpVM vm
-      throwE $ "Invalid input modes: " ++ show modes ++ " " ++ dump
-
-outputOp :: IntcodeOp
-outputOp = IntcodeOp outputOp' 1
-  where
-    outputOp' :: forall s. Op s
-    outputOp' [mode] = do
-      vm@IntcodeVM{outputBuf} <- ask
-      x <- withMode mode
-      lift . lift $ modifySTRef outputBuf (x:)
-    outputOp' modes = ReaderT $ \vm -> do
-      dump <- lift $ dumpVM vm
-      throwE $ "Invalid output: " ++ show modes ++ " " ++ dump
-
-halt :: IntcodeOp
-halt = IntcodeOp halt' 0
-  where
-    halt' [] = ReaderT $ \IntcodeVM{halted} -> lift $ writeSTRef halted Halt
-    halt' modes = ReaderT $ \vm -> do
-      dump <- lift $ dumpVM vm
-      throwE $ "Trying to halt with modes: " ++ show modes ++ " " ++ dump
-
-jumpConditional :: (Int -> Bool) -> IntcodeOp
-jumpConditional f = IntcodeOp jumpIfTrue' 2
-  where
-    jumpIfTrue' :: forall s. Op s
-    jumpIfTrue' [firstMode, secondMode] = do
-      vm@IntcodeVM{..} <- ask
-      check <- withMode firstMode
-      jumpTo <- withMode secondMode
-      when (f check) . lift . lift $ writeSTRef instPointer jumpTo
-
-writeConditional :: (Int -> Int -> Bool) -> IntcodeOp
-writeConditional f = IntcodeOp writeConditional' 3
-  where
-    writeConditional' :: forall s. Op s
-    writeConditional' [firstMode,secondMode,Pos] = do
-      vm@IntcodeVM{..} <- ask
-      first <- withMode firstMode
-      second <- withMode secondMode
-      writeTo <- withMode Immed
-      MVector.write code writeTo (fromEnum $ first `f` second)
-    writeConditional' modes = ReaderT $ \vm -> do
-      dump <- lift $ dumpVM vm
-      throwE $ "Attempting conditional write with immediate mode: " ++ show modes ++ " " ++ dump
-
-ops :: IntMap IntcodeOp
-ops = IntMap.fromList
-  [ (1, toIntcodeOp ((+) :: Int -> Int -> Int))
-  , (2, toIntcodeOp ((*) :: Int -> Int -> Int))
-  , (3, inputOp)
-  , (4, outputOp)
-  , (5, jumpConditional (/= 0))
-  , (6, jumpConditional (== 0))
-  , (7, writeConditional (<))
-  , (8, writeConditional (==))
-  , (99, halt)
-  ]
-
-modifyIntcode :: Vector Int -> [(Int, Int)] -> ST s (Code s)
-modifyIntcode vec subs = do
-  mutvec <- Vector.thaw vec
-  mapM_ (uncurry (MVector.write mutvec)) subs
-  return mutvec
-
-parseOpCode :: Monad m => Int -> ExceptT String m (Int, [Mode])
-parseOpCode n =
-  let digitsRev = reverse . show $ n
-      opCode = read @Int . reverse . take 2 $ digitsRev
-      toMode '0' = pure Pos
-      toMode '1' = pure Immed
-      toMode c = throwE $ "Invalid mode code: " ++ show c ++ " in " ++ show n ++ "\n"
-      modes = traverse toMode . drop 2 $ digitsRev
-   in sequenceA (opCode, modes)
-
-runIntcodeOp :: Interpreter s ()
-runIntcodeOp = do
-  vm <- ask
-  opCode <- withMode Immed
-  parseOp <-
-    lift $
-    catchE
-      (parseOpCode opCode)
-      (\err -> do
-         dump <- lift $ dumpVM vm
-         throwE $ err ++ "\tDump: " ++ dump ++ "\n")
-  let (opCode, modes) = parseOp
-  op <-
-    lift $
-    maybeToExceptT ("OpCode " ++ show opCode ++ " not recognised") .
-    MaybeT . pure $
-    IntMap.lookup opCode ops
-  let modesPadded = modes ++ replicate (icopParams op - length modes) Pos
-  icopRun op modesPadded
-
-initVM :: (forall s. (ST s) (Code s)) -> [Input] -> ST s (IntcodeVM s)
-initVM mcode inputs = do
-  instPointer <- newSTRef 0
-  inputBuf <- newSTRef inputs
-  outputBuf <- newSTRef []
-  halted <- newSTRef Run
-  code <- mcode
-  return $ IntcodeVM {..}
-
-runIntcode :: forall s. Interpreter s (Addr, [Input], [Output], Vector Int)
-runIntcode = do
-  run
-  vm@IntcodeVM{..} <- ask
-  finalPtr <- lift. lift $ readSTRef instPointer
-  finalCode <- Vector.freeze code
-  finalInput <- lift . lift $ readSTRef inputBuf
-  finalOutput <- lift. lift $ readSTRef outputBuf
-  return (finalPtr, finalInput, finalOutput, finalCode)
-    where
-      run = do
-        vm@IntcodeVM{..} <- ask
-        haltedNow <- lift . lift $ readSTRef halted
-        currentPtr <- lift . lift $ readSTRef instPointer
-        unless (haltedNow == Halt) $
-          if haltedNow == Run && (currentPtr < 0 || currentPtr >= MVector.length code)
-             then lift . throwE $ "Attempting to access instruction out of memory at: " ++ show currentPtr
-             else runIntcodeOp >> run
-
-initAndRun :: Monad m => (forall s. (ST s) (Code s)) -> [Input] -> ExceptT String m (Addr, [Input], [Output], Vector Int)
-initAndRun mcode inputs = except $ runST (runExceptT go)
-  where
-    go = do
-      vm <- lift $ initVM mcode inputs
-      runReaderT runIntcode vm
-
-readIntCode :: Monad m => Text.Text -> ExceptT String m (Vector Int)
-readIntCode text =
-  let codeOps = Text.splitOn "," text
-      intCodeList = except $ traverse (fmap fst . Text.Read.signed Text.Read.decimal) codeOps
-   in Vector.fromList <$> intCodeList
-
-solve' :: ExceptT String IO ()
-solve' = do
-  codeText <- lift Text.IO.getContents
-  intCodeVec <- readIntCode codeText
-  (ptr, input, output, code) <- initAndRun (modifyIntcode intCodeVec []) [1]
-  lift . print $ reverse output
-  (ptr, input, output, code) <- initAndRun (modifyIntcode intCodeVec []) [5]
-  lift . print $ reverse output
-  pure ()
+solve' :: Code -> Int -> IO ()
+solve' intCode x =
+  (>> pure ()) . runExceptT $ do
+    (state, vm) <-
+      S.liftIO $
+      SP.print $ S.hoist (pure . runIdentity) $ runProgram intCode (SP.each [x])
+    case state of
+      Left err -> S.liftIO $ putStrLn err
+      Right () -> pure ()
 
 solve :: IO ()
 solve = do
-  result <- runExceptT solve'
-  case result of
+  codeText <- Text.IO.getContents
+  codeParse <- runExceptT (readIntCode codeText)
+  case codeParse of
     Left err -> putStrLn err
-    Right () -> pure ()
+    Right intCode -> solve' intCode 1 >> solve' intCode 5
---
+++ src/Intcode.hs
@@ -0,0 +1,261 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE TypeOperators #-}
+
+module Intcode
+  ( Code
+  , VMResult
+  , IntCodeVM
+  , InputStream
+  , OutputStream
+  , readIntCode
+  , modifyIntcode
+  , runProgram
+  , module Control.Monad.Trans.Class
+  , module Data.Functor.Identity
+  , module Control.Monad.Trans.Except
+  ) where
+
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.State
+import Data.Text (Text)
+import qualified Data.Text as Text
+import qualified Data.Text.IO as Text.IO
+import qualified Data.Text.Read as Text.Read
+import Data.Proxy
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IntMap
+import Control.Foldl (Fold(..))
+import qualified Control.Foldl as Foldl
+import Streaming (Of, Stream)
+import qualified Streaming as S
+import qualified Streaming.Prelude as SP
+import Data.Functor.Identity
+
+data Halt = Run | Halt deriving (Show, Read, Eq)
+data Mode = Positional | Immediate deriving (Show, Read, Eq)
+
+type Code = IntMap Int
+type Addr = Int
+type Input = Int
+type Output = Int
+type InputStream = Stream (Of Int) Identity ()
+type VMResult = (Either String (), IntCodeVM)
+type OutputStream = Stream (Of Int) Identity VMResult
+type Interpreter = ExceptT String (State IntCodeVM)
+type Op = [Mode] -> Interpreter (Maybe Int)
+
+data IntCodeOp = IntCodeOp
+  { intCodeOp :: Op
+  , paramCount :: Int
+  }
+
+data IntCodeVM = IntCodeVM
+  { haltState :: Halt
+  , code :: Code
+  , instrPointer :: Int
+  , inputBuf :: InputStream
+  }
+
+instance Show IntCodeVM where
+  show vm@IntCodeVM{..} =
+    let haltS = "Halt state: " ++ show haltState
+        addrS = "At address: " ++ show instrPointer
+        codeS = "With code: " ++ show code
+        inputS = "With unprocessed inputs: " ++ show inputBuf
+     in unlines [haltS, addrS, codeS, inputS]
+
+class ToIntCodeOp t where
+  countParams :: proxy t -> Int
+  toOp :: t -> Op
+
+instance ToIntCodeOp Int where
+  countParams _ = 1
+  toOp x [mode] = do
+    vm@IntCodeVM{..} <- lift get
+    write x mode
+    return Nothing
+  toOp x modes = do
+    vm <- lift get
+    throwE $ "Leftover modes: " ++ show modes ++ "\n" ++ show vm
+
+instance ToIntCodeOp r => ToIntCodeOp (Int -> r) where
+  countParams _ = 1 + countParams (Proxy @r)
+  toOp f [] = do
+    vm <- lift get
+    throwE $ "Ran out of modes.\n" ++ show vm
+  toOp f (m:ms) = do
+    x <- readWithMode m
+    toOp (f x) ms
+
+toIntcodeOp :: forall f. ToIntCodeOp f => f -> IntCodeOp
+toIntcodeOp f = IntCodeOp (toOp f) (countParams (Proxy @f))
+
+wrapMaybe str = maybeToExceptT str . MaybeT . pure
+
+readWithMode :: Mode -> Interpreter Int
+readWithMode Positional = do
+  vm@IntCodeVM{..} <- lift get
+  addr <- wrapMaybe ("Attempt to access memory failed.\n" ++ show vm) $ IntMap.lookup instrPointer code
+  val <- wrapMaybe ("Attempt to access memory failed.\n" ++ show vm) $ IntMap.lookup addr code
+  lift $ put vm {instrPointer = succ instrPointer}
+  return val
+readWithMode Immediate = do
+  vm@IntCodeVM{..} <- lift get
+  val <- wrapMaybe ("Attempt to access memory failed.\n" ++ show vm) $ IntMap.lookup instrPointer code
+  lift $ put vm {instrPointer = succ instrPointer}
+  return val
+
+write :: Int -> Mode -> Interpreter ()
+write x Positional = do
+  vm@IntCodeVM{..} <- lift get
+  addr <- wrapMaybe ("Attempt to access memory failed.\n" ++ show vm) $ IntMap.lookup instrPointer code
+  let newCode = IntMap.insert addr x code
+      advPointer = succ instrPointer
+  lift $ put vm {code = newCode, instrPointer = advPointer}
+  return ()
+write x Immediate = do
+  vm <- lift get
+  throwE $ "Attempt to write with immediate mode.\n" ++ show vm
+
+acceptInput :: Interpreter Int
+acceptInput = do
+  vm@IntCodeVM {..} <- lift get
+  (x, restOfInput) <-
+    wrapMaybe
+      ("Attemping to get input from an empty buffer.\n" ++ show vm) $
+    runIdentity (SP.uncons inputBuf)
+  lift $ put vm {inputBuf = restOfInput}
+  return x
+
+inputOp :: IntCodeOp
+inputOp = IntCodeOp inputOp' 1
+  where
+    inputOp' [mode] = do
+      x <- acceptInput
+      write x mode
+      return Nothing
+    inputOp' modes = do
+      vm <- lift get
+      throwE $ "Invalid input modes: " ++ show modes ++ "\n" ++ show vm
+
+outputOp :: IntCodeOp
+outputOp = IntCodeOp outputOp' 1
+  where
+    outputOp' [mode] = do
+      x <- readWithMode mode
+      return $ Just x
+    outputOp' modes = do
+      vm <- lift get
+      throwE $ "Invalid output: " ++ show modes ++ "\n" ++ show vm
+
+halt :: IntCodeOp
+halt = IntCodeOp halt' 0
+  where
+    halt' [] = do
+      vm <- lift get
+      lift $ put vm {haltState = Halt}
+      return Nothing
+    halt' modes = do
+      vm <- lift get
+      throwE $ "Trying to halt with modes: " ++ show modes ++ "\n" ++ show vm
+
+jumpConditional :: (Int -> Bool) -> IntCodeOp
+jumpConditional f = IntCodeOp jumpIfTrue' 2
+  where
+    jumpIfTrue' [firstMode, secondMode] = do
+      vm <- lift get
+      check <- readWithMode firstMode
+      jumpTo <- readWithMode secondMode
+      if f check then lift $ put vm {instrPointer = jumpTo}
+                 else pure ()
+      return Nothing
+    jumpIfTrue' modes = do
+      vm <- lift get
+      throwE $ "Conditional jump with invalid modes: " ++ show modes ++ "\n" ++ show vm
+
+writeConditional :: (Int -> Int -> Bool) -> IntCodeOp
+writeConditional f = IntCodeOp writeConditional' 3
+  where
+    writeConditional' [firstMode,secondMode,thirdMode] = do
+      first <- readWithMode firstMode
+      second <- readWithMode secondMode
+      write (fromEnum $ first `f` second) thirdMode
+      return Nothing
+    writeConditional' modes = do
+      vm <- lift get
+      throwE $ "Attempting conditional write with immediate mode: " ++ show modes ++ "\n" ++ show vm
+
+ops :: IntMap IntCodeOp
+ops = IntMap.fromList
+  [ (1, toIntcodeOp ((+) :: Int -> Int -> Int))
+  , (2, toIntcodeOp ((*) :: Int -> Int -> Int))
+  , (3, inputOp)
+  , (4, outputOp)
+  , (5, jumpConditional (/= 0))
+  , (6, jumpConditional (== 0))
+  , (7, writeConditional (<))
+  , (8, writeConditional (==))
+  , (99, halt)
+  ]
+
+modifyIntcode :: Code -> [(Int, Int)] -> Code
+modifyIntcode code = Foldl.fold map
+  where
+    map = Fold step code id
+    step m (k, v) = IntMap.insert k v m
+
+parseOpCode :: Monad m => Int -> ExceptT String m (Int, [Mode])
+parseOpCode n =
+  let digitsRev = reverse . show $ n
+      opCode = read @Int . reverse . take 2 $ digitsRev
+      toMode '0' = pure Positional
+      toMode '1' = pure Immediate
+      toMode c = throwE $ "Invalid mode code: " ++ show c ++ " in " ++ show n ++ "\n"
+      modes = traverse toMode . drop 2 $ digitsRev
+   in sequenceA (opCode, modes)
+
+runIntcodeOp :: Interpreter (Maybe Int)
+runIntcodeOp = do
+  vm <- lift get
+  opCode <- readWithMode Immediate
+  parseOp <-
+    catchE
+      (parseOpCode opCode)
+      (\err -> throwE $ err ++ "\tDump: " ++ show vm ++ "\n")
+  let (opCode, modes) = parseOp
+  op <- wrapMaybe ("OpCode " ++ show opCode ++ " not recognised") $ IntMap.lookup opCode ops
+  let modesPadded = modes ++ replicate (paramCount op - length modes) Positional
+  intCodeOp op modesPadded
+
+runProgram :: Code -> InputStream -> OutputStream
+runProgram code inputs = finalStream $ IntCodeVM Run code 0 inputs
+  where
+    oneOp :: Interpreter (Either (Maybe Int) ())
+    oneOp = do
+      output <- runIntcodeOp
+      vm@IntCodeVM{..} <- lift get
+      return $ case haltState of
+                 Halt -> Right ()
+                 Run -> Left output
+    opStream :: Stream (Of Int) Interpreter ()
+    opStream = SP.catMaybes $ SP.untilRight oneOp
+    stateStream = runExceptT (S.distribute opStream)
+    finalStream = runStateT (S.distribute stateStream)
+
+readIntCode :: Monad m => Text.Text -> ExceptT String m Code
+readIntCode text =
+  let codeOps = Text.splitOn "," text
+      intCodeList = except $ traverse (fmap fst . Text.Read.signed Text.Read.decimal) codeOps
+   in IntMap.fromList . zip [0..] <$> intCodeList
