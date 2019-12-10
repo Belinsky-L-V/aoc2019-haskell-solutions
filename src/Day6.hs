c4c7d43dc2478db1f856f39d4eb3d877c1b75405
Belinsky-L-V <belinsky.leonid@gmail.com>
2019-12-10 23:35:40 +0300

    unpotato day 6 performance

--- src/Day6.hs
+++ src/Day6.hs
@@ -2,16 +2,26 @@ module Day6 (solve6) where
 
 import qualified Data.Graph.Inductive as FGL
 import Data.Graph.Inductive.PatriciaTree (Gr)
-import Data.List (foldl', span)
+import Data.List (span)
 import System.IO (Handle, hGetContents)
 
-part1 :: Gr String () -> Int
-part1 = FGL.size . FGL.tc
+part1 :: Gr String () -> FGL.NodeMap String -> Maybe Int
+part1 gr nodeMap = do
+  let (root,_) = FGL.mkNode_ nodeMap "COM"
+      go graph node = do
+        let (mcontext, forest) = FGL.match node graph
+        (_, _, _, children) <- mcontext
+        if null children
+           then return (1, 0)
+           else do
+             (sizes, links) <- unzip <$> traverse (\(_,child) -> go forest child) children
+             return (1 + sum sizes, sum sizes + sum links)
+  snd <$> go gr root
 
 part2 :: Gr String () -> FGL.NodeMap String -> Int
 part2 gr nodeMap =
-  let ((you,_),_) = FGL.mkNode nodeMap "YOU"
-      ((san,_),_) = FGL.mkNode nodeMap "SAN"
+  let (you,_) = FGL.mkNode_ nodeMap "YOU"
+      (san,_) = FGL.mkNode_ nodeMap "SAN"
       youOrb = head $ FGL.pre gr you
       sanOrb = head $ FGL.pre gr san
    in length (FGL.esp youOrb sanOrb (FGL.undir gr)) - 1
@@ -32,5 +42,8 @@ solve6 :: Handle -> IO String
 solve6 handle = do
   input <- parseInput handle
   case toGraph input of
-    (Nothing, _) -> return "Failed to construct the graph from input :("
-    (Just graph, nodeMap) -> return . unlines . map show $ [part1 graph, part2 graph nodeMap]
+    (Nothing, _) -> return "Failed to construct the graph from input :(\n"
+    (Just graph, nodeMap) -> do
+      let part1out = maybe "Part 1 failed :(\n" show (part1 graph nodeMap)
+          part2out = show $ part2 graph nodeMap
+      return $ unlines [part1out, part2out]
